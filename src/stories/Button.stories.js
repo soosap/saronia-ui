24151351b12e1fc969c22561709719b25da59755
soosap <prasath.soosaithasan@gmail.com>
2017-03-28 11:41:13 +0200

    Add support for storybook knobs

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,11 +1,19 @@
 /* @flow */
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { withKnobs, text, boolean, number } from '@kadira/storybook-addon-knobs';
+
 import Button from '../components/Button';
 
-storiesOf('Button', module)
+const stories = storiesOf('Button', module);
+
+// Add the `withKnobs` decorator to add knobs support to your stories.
+// You can also configure `withKnobs` as a global decorator.
+stories.addDecorator(withKnobs);
+
+stories
   .add('with text', () => (
-    <Button accent onClick={action('clicked')}>Hello Button</Button>
+    <Button accent onClick={action('clicked')}>{text('DisplayText', 'Accent Button')}</Button>
   ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
