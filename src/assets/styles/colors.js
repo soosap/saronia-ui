64f4e892d53f630c033b79995c1613227ce53826
soosap <prasath.soosaithasan@gmail.com>
2017-04-13 18:02:50 +0200

    Specify grey, white, and ivory color codes

--- src/assets/styles/colors.js
+++ src/assets/styles/colors.js
@@ -1,10 +1,19 @@
 /* @flow */
 export const primary = '#FFB00C';
 export const primaryDark = '#FFA104';
+
 export const secondary = '#F2416C';
 export const secondaryDark = '#D3395E';
+
+export const grey = '#CDCDCB';
+export const greyDark = '#BABCB9'
+
+export const white = '#FFFFFF';
+export const whiteDark = '#FAFAF7';
+export const ivory = '#FDFDF5';
+export const ivoryDark = '#FFFEF0';
+
 export const black = '#161616';
-export const white = '#FFFEF0';
 export const transparent = 'transparent';
 
 export default {
@@ -12,7 +21,12 @@ export default {
   primaryDark,
   secondary,
   secondaryDark,
-  black,
   white,
+  whiteDark,
+  ivory,
+  ivoryDark,
+  grey,
+  greyDark,
+  black,
   transparent,
 }
