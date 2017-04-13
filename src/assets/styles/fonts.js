75b9d37362c83d4406f12bbb72810b29eafd6709
Daniyar <ddr199305@gmail.com>
2017-04-13 17:24:04 +0200

    Add a set of different font sizes

--- src/assets/styles/fonts.js
+++ src/assets/styles/fonts.js
@@ -2,6 +2,17 @@
 const system = 'PT Sans';
 const accent = 'Satisfy';
 
+const sizes = {
+   'mini': '0.7rem',
+   'tiny': '0.8rem',
+   'small': '0.9rem',
+   'medium': '1rem',
+   'large': '1.1rem',
+   'big': '1.3rem',
+   'huge': '1.5rem',
+   'massive': '2rem'
+}
+
 export default {
   system,
   accent,
