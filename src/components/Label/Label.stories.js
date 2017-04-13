b84d7f1ce0d09b43a62a33c9116e6d102d7091da
soosap <prasath.soosaithasan@gmail.com>
2017-04-13 23:08:45 +0200

    Run through prettier

--- src/components/Label/Label.stories.js
+++ src/components/Label/Label.stories.js
@@ -13,35 +13,32 @@ import Label from '.';
 
 const stories = storiesOf('Label', module);
 stories.addDecorator(withKnobs);
-stories.addDecorator((story) => (
-   <div style={{margin: '50px'}}>
-      {story()}
-   </div>
-))
+stories.addDecorator(story => (
+  <div style={{ margin: '50px' }}>
+    {story()}
+  </div>
+));
 
 const arrowOptions = {
   left: 'Left',
-  right: 'Right'
+  right: 'Right',
 };
 
 const labelSizes = {
-   mini: 'Mini',
-   tiny: 'Tiny',
-   small: 'Small',
-   medium: 'Medium',
-   large: 'Large',
-   big: 'Big',
-   huge: 'Huge',
-   massive: 'Massive'
+  mini: 'Mini',
+  tiny: 'Tiny',
+  small: 'Small',
+  medium: 'Medium',
+  large: 'Large',
+  big: 'Big',
+  huge: 'Huge',
+  massive: 'Massive',
 };
 
-stories
-   .add('default', () => (
-   <Label
-      size={select('Size', labelSizes, 'tiny')}
-   >
-       MyLabel
-    </Label>
+stories.add('default', () => (
+  <Label size={select('Size', labelSizes, 'tiny')}>
+    MyLabel
+  </Label>
 ));
 //   .add('arrows', () => (
 //     <Label
--- src/components/Label/index.js
+++ src/components/Label/index.js
@@ -5,53 +5,34 @@ import styled from 'styled-components';
 import { colors, fonts, borders } from '../../assets/styles';
 
 type Props = {
-   tag?: boolean,
-   arrow?: 'left' | 'right',
-   size?: 'mini' | 'tiny' | 'small' | 'medium' | 'large' | 'big' | 'huge' | 'massive',
+  tag?: boolean,
+  arrow?: 'left' | 'right',
+  size?:
+    | 'mini'
+    | 'tiny'
+    | 'small'
+    | 'medium'
+    | 'large'
+    | 'big'
+    | 'huge'
+    | 'massive',
 };
 
 const getBackgroundColor = R.cond([
-  [
-    R.prop('tag'),
-    R.always(colors.grey)
-  ],
+  [R.prop('tag'), R.always(colors.grey)],
   [R.T, R.always(colors.grey)],
 ]);
 
 const getSize = R.cond([
-      [
-         R.propEq('size', 'mini'),
-         R.always(fonts.size.mini)
-      ],
-      [
-         R.propEq('size', 'tiny'),
-         R.always(fonts.size.tiny)
-      ],
-      [
-         R.propEq('size', 'small'),
-         R.always(fonts.size.small)
-      ],
-      [
-         R.propEq('size', 'medium'),
-         R.always(fonts.size.medium)
-      ],
-      [
-         R.propEq('size', 'large'),
-         R.always(fonts.size.large)
-      ],
-      [
-         R.propEq('size', 'big'),
-         R.always(fonts.size.big)
-      ],
-      [
-         R.propEq('size', 'huge'),
-         R.always(fonts.size.huge)
-      ],
-      [
-         R.propEq('size', 'massive'),
-         R.always(fonts.size.massive)
-      ]
-])
+  [R.propEq('size', 'mini'), R.always(fonts.size.mini)],
+  [R.propEq('size', 'tiny'), R.always(fonts.size.tiny)],
+  [R.propEq('size', 'small'), R.always(fonts.size.small)],
+  [R.propEq('size', 'medium'), R.always(fonts.size.medium)],
+  [R.propEq('size', 'large'), R.always(fonts.size.large)],
+  [R.propEq('size', 'big'), R.always(fonts.size.big)],
+  [R.propEq('size', 'huge'), R.always(fonts.size.huge)],
+  [R.propEq('size', 'massive'), R.always(fonts.size.massive)],
+]);
 
 const Label = styled.label`
   padding:  0px 6px;
@@ -69,33 +50,34 @@ const Label = styled.label`
   }
 
   &:after {
-     content: "";
-     position: absolute;
-     top: 0;
-     left: ${props => props.arrow=='right' ? '100%' : 0};
-     height: 0;
-     width: 0;
-     border-width: ${props => props.arrow=='right' ? fonts.sizes.small : 0};
-     border-style: solid;
-     border-color: ${getBackgroundColor};
-     border-top-color:transparent;
-     border-bottom-color:transparent;
-     border-right-color:transparent;
- }
- &:before {
     content: "";
     position: absolute;
     top: 0;
-    right: ${props => props.arrow=='left' ? '100%' : 0};
+    left: ${props => props.arrow == 'right' ? '100%' : 0};
     height: 0;
     width: 0;
-    border-width: ${props => props.arrow=='left' ? '0.5rem' : 0};
+    border-width: ${props => props.arrow == 'right' ? fonts.sizes.small : 0};
+    border-style: solid;
+    border-color: ${getBackgroundColor};
+    border-top-color:transparent;
+    border-bottom-color:transparent;
+    border-right-color:transparent;
+ }
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: ${props => props.arrow == 'left' ? '100%' : 0};
+    height: 0;
+    width: 0;
+    border-width: ${props => props.arrow == 'left' ? '0.5rem' : 0};
     border-style: solid;
     border-color: ${getBackgroundColor};
     border-top-color:transparent;
     border-bottom-color:transparent;
     border-left-color:transparent;
-}
+  }
 `;
 
 export default Label;
