5a671b707a9b4265a53d8e8720df0e127ec30da0
Daniyar <ddr199305@gmail.com>
2017-04-14 11:24:56 +0200

    Change font sizes

--- src/assets/styles/fonts.js
+++ src/assets/styles/fonts.js
@@ -3,18 +3,18 @@ const system = 'PT Sans';
 const accent = 'Satisfy';
 
 const sizes = {
-  mini: '0.7rem',
-  tiny: '0.8rem',
-  small: '0.9rem',
-  medium: '1rem',
-  large: '1.1rem',
-  big: '1.3rem',
-  huge: '1.5rem',
-  massive: '2rem',
+   mini: '0.64285714rem',
+   tiny: '0.71428571rem',
+   small: '0.78571429rem',
+   medium: '0.85714286rem',
+   large: '1rem',
+   big: '1.28571429rem',
+   huge: '1.42857143rem',
+   massive: '1.71428571rem',
 };
 
 export default {
-  system,
-  accent,
-  sizes,
+   system,
+   accent,
+   sizes,
 };
