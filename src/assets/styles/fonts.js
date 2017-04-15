d1ae9a41da30eba8c4f340d3829a507948d4762b
soosap <prasath.soosaithasan@gmail.com>
2017-04-15 20:55:35 +0200

    Pick sensible font size values

--- src/assets/styles/fonts.js
+++ src/assets/styles/fonts.js
@@ -5,13 +5,13 @@ const url = 'https://fonts.googleapis.com/css?family=Quicksand:300,400|Satisfy';
 
 const sizes = {
   mini: '0.64285714rem',
-  tiny: '0.71428571rem',
-  small: '0.78571429rem',
-  medium: '0.85714286rem',
-  large: '1rem',
-  big: '1.28571429rem',
-  huge: '1.42857143rem',
-  massive: '1.71428571rem',
+  tiny: '0.78571429rem',
+  small: '0.85714286rem',
+  medium: '1rem',
+  large: '1.42857143rem',
+  big: '1.92857142rem',
+  huge: '2.35714287rem',
+  massive: '3rem',
 };
 
 export default {
