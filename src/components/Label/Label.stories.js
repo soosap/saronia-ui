d17863c7aa47e79ebb1e25ee8b5e5f67a64fb86c
Daniyar <ddr199305@gmail.com>
2017-04-16 09:40:52 +0200

    Add getFontSize() & remove camelCase in CSS

--- src/components/Label/Label.stories.js
+++ src/components/Label/Label.stories.js
@@ -1,6 +1,7 @@
 /* @flow */
 import React from 'react';
 import R from 'ramda';
+import styled from 'styled-components';
 
 import { storiesOf, action, linkTo } from '@kadira/storybook';
 import {
@@ -19,11 +20,16 @@ import Label from '.';
 
 const stories = storiesOf('Label', module);
 stories.addDecorator(withKnobs);
-stories.addDecorator(story => (
-  <div style={{ margin: '50px' }}>
-    {story()}
-  </div>
-));
+
+const Wrapper = styled.div`
+  display: flex-inline;
+  flex-direction: column;
+  width: 250px;
+`;
+const Segment = styled.div`
+height: 50px;
+  background-color: red;
+`;
 
 stories
   .add('default', () => (
@@ -37,13 +43,16 @@ stories
     </Label>
   ))
   .add('arrow', () => (
-    <Label
-      arrow={select('arrow', positionOptions, 'right')}
-      size={select('size', sizeOptions, 'medium')}
-      primary={boolean('primary', false)}
-      secondary={boolean('secondary', false)}
-      inverted={boolean('inverted', false)}
-    >
-      {text('display', 'Enter a value')}
-    </Label>
+    <Wrapper>
+      <Segment></Segment>
+      <Label
+        arrow={select('arrow', positionOptions, 'right')}
+        size={select('size', sizeOptions, 'medium')}
+        primary={boolean('primary', false)}
+        secondary={boolean('secondary', false)}
+        inverted={boolean('inverted', false)}
+      >
+        {text('display', 'Enter a value')}
+      </Label>
+    </Wrapper>
   ));
--- src/components/Label/index.js
+++ src/components/Label/index.js
@@ -52,7 +52,19 @@ const getBorderColor = R.cond([
     R.both(R.propEq('primary', false), R.propEq('secondary', false)),
     R.always(colors.grey),
   ],
-  [R.T, R.always(colors.transparent)],
+  [R.T, R.always( 'transparent')],
+]);
+
+const getFontColor = R.cond([
+  [
+    R.both(R.propEq('inverted', true), R.propEq('primary', true)),
+    R.always(colors.primary),
+  ],
+  [
+    R.both(R.propEq('inverted', true), R.propEq('secondary', true)),
+    R.always(colors.secondary),
+  ],
+  [R.T, R.always('rgba(0, 0, 0, 0.6)')],
 ]);
 
 const Label = styled.label`
@@ -63,7 +75,7 @@ const Label = styled.label`
    background-color: ${getBackgroundColor};
    background-image: none;
    padding: 0.5833em 0.833em;
-   color: rgba(0, 0, 0, 0.6);
+   color: ${getFontColor};
    text-transform: none;
    font-size: ${getSize};
    font-family: ${fonts.system};
@@ -95,29 +107,27 @@ const Label = styled.label`
 `;
 
 const labelBefore = {
-  backgroundColor: 'inherit',
-  backgroundImage: 'inherit',
-  borderWidth: borders.width.thin,
-  borderStyle: 'solid',
-  borderColor: 'inherit',
+  'background-color': 'inherit',
+  'background-image': 'inherit',
+  'border-width': borders.width.thin,
+  'border-style': 'solid',
+  'border-color': 'inherit',
 };
 const arrowLabelBefore = {
   position: 'absolute',
   content: '""',
-  WebkitTransform: 'rotate(45deg)',
   transform: 'rotate(45deg)',
-  backgroundImage: 'none',
-  zIndex: '2',
+  'background-image': 'none',
+  'z-index': '2',
   width: '0.6666em',
   height: '0.6666em',
 };
 const leftArrow = {
-  marginTop: '0em',
-  marginLeft: '0.6666em',
+  'margin-top': '0em',
+  'margin-left': '0.5em',
 };
 const leftArrowBefore = {
-  borderWidth: '0px 0px 1px 1px',
-  WebkitTransform: 'translateX(-50%) translateY(-50%) rotate(45deg)',
+  'border-width': '0px 0px 1px 1px',
   transform: 'translateX(-50%) translateY(-50%) rotate(45deg)',
   bottom: 'auto',
   right: 'auto',
@@ -125,12 +135,11 @@ const leftArrowBefore = {
   left: '0em',
 };
 const rightArrow = {
-  marginTop: '0em',
-  marginRight: '0.6666em',
+  'margin-top': '0em',
+  'margin-right': '0.5em',
 };
 const rightArrowBefore = {
-  borderWidth: '1px 1px 0px 0px',
-  WebkitTransform: 'translateX(50%) translateY(-50%) rotate(45deg)',
+  'border-width': '1px 1px 0px 0px',
   transform: 'translateX(50%) translateY(-50%) rotate(45deg)',
   bottom: 'auto',
   right: '0%',
@@ -138,22 +147,20 @@ const rightArrowBefore = {
   left: 'auto',
 };
 const topArrow = {
-  marginTop: '1em',
+  'margin-top': '0.5em',
 };
 const topArrowBefore = {
-  borderWidth: '1px 0px 0px 1px',
-  WebkitTransform: 'translateX(-50%) translateY(-50%) rotate(45deg)',
+  'border-width': '1px 0px 0px 1px',
   transform: 'translateX(-50%) translateY(-50%) rotate(45deg)',
   top: '0',
   left: '50%',
 };
 const bottomArrow = {
-  marginTop: '0em',
-  marginBottom: '1em',
+  'margin-top': '0em',
+  'margin-bottom': '0.5em',
 };
 const bottomArrowBefore = {
-  borderWidth: '0px 1px 1px 0px',
-  WebkitTransform: 'translateX(-50%) translateY(-50%) rotate(45deg)',
+  'border-width': '0px 1px 1px 0px',
   transform: 'translateX(-50%) translateY(-50%) rotate(45deg)',
   top: '100%',
   left: '50%',
