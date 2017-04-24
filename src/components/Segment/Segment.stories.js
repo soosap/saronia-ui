0077024d6e87d3a70930b16b46c4618d0ce186b7
Daniyar <ddr199305@gmail.com>
2017-04-24 20:08:56 +0200

    Add piled to emphasis prop

--- src/components/Segment/Segment.stories.js
+++ src/components/Segment/Segment.stories.js
@@ -17,7 +17,7 @@ import { Segment } from '.';
 const stories = storiesOf('Segment', module);
 stories.addDecorator(withKnobs);
 
-const typeOptions = R.invertObj(R.merge(BreedEnum, { DEFAULT: undefined }));
+const typeOptions = R.invertObj(R.merge({ DEFAULT: undefined }, BreedEnum));
 const paddingOptions = R.invertObj(SizeEnum);
 const emphasisOptions = R.invertObj({
   DEFAULT: undefined,
@@ -31,14 +31,14 @@ stories.add('default', () => {
     'children',
     `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
   );
-
+  const basic = boolean('basic', false);
   const props = {
-    basic: boolean('basic', false),
     type: select('type', typeOptions, undefined),
-    inverted: boolean('inverted', false),
     padding: select('padding', paddingOptions, 'medium'),
     compact: boolean('compact', false),
-    emphasis: select('emphasis', emphasisOptions, undefined),
+    basic,
+    inverted: basic ? undefined : boolean('inverted', false),
+    emphasis: basic ? undefined: select('emphasis', emphasisOptions, undefined),
   };
 
   return (
