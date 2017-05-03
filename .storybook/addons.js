2ee1499a2fedb04bb0889ad7dcd843c4e138e9bf
soosap <prasath.soosaithasan@gmail.com>
2017-05-03 11:52:19 +0200

    Add custom styles to storybook-ui

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,6 @@
 //  To get our default addons (actions and links)
 import '@kadira/storybook/addons';
-//  To add the knobs addon
+//  Add the knobs addon
 import '@kadira/storybook-addon-knobs/register';
+// Add the options addon to customize the storybook-ui
+import '@kadira/storybook-addon-options/register';
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@kadira/storybook';
+import { setOptions } from '@kadira/storybook-addon-options';
 import { generateAndInjectGlobalCSS } from '../src';
 
 generateAndInjectGlobalCSS();
@@ -8,5 +9,10 @@ const req = require.context('../src/components', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+setOptions({
+  name: 'saronia-ui',
+  url: 'https://saronia.io/ui'
+});
 
 configure(loadStories, module);
