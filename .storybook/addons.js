d127244dc8eccaa7123ed8768b07e1c8ffcdeea3
soosap <prasath.soosaithasan@gmail.com>
2017-05-27 00:47:57 +0200

    Fix: add missing @storybook/addons package

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,5 @@
+//  To get our default addons (actions and links)
+import '@storybook/addons';
 //  Add the knobs addon
 import '@storybook/addon-knobs/register'
 // Add the options addon to customize the storybook-ui
