a6031768ed689ff782959f80e7dc47d329bc1dfd
soosap <prasath.soosaithasan@gmail.com>
2017-05-27 11:05:47 +0200

    Order props in <Input /> component

--- src/components/forms/Field/Field.js
+++ src/components/forms/Field/Field.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 type Props = {
-  inline: boolean,
+  inline?: boolean,
 };
 
 const Field = styled.label`
--- src/components/forms/Input/Input.js
+++ src/components/forms/Input/Input.js
@@ -11,17 +11,17 @@ type Props = {
 
 const Input = styled.input`
   display: inline-flex;
-  height: 2rem;
-  outline: none;
-  border: 1px solid transparent;
-  line-height: 2rem;
-  vertical-align: middle;
-  border-radius: ${BORDER_RADIUS};
   font-size: 1rem;
   padding: 0 .5rem;
-  margin-bottom: .2rem;
-  box-shadow: inset 0 1px 2px rgba(10, 10, 10, 0.1);
+  margin-bottom: .5rem;
+  height: 2rem;
+  line-height: 2rem;
+  vertical-align: middle;
+  outline: none;
+  border: 1px solid transparent;
+  border-radius: ${BORDER_RADIUS};
   border-color: #dbdbdb;
+  box-shadow: inset 0 1px 2px rgba(10, 10, 10, 0.1);
   transition: box-shadow 100ms cubic-bezier(0.4, 1, 0.75, 0.9);
 
   &::placeholder {
