6d65364814e81862a72367c9f5b18b5c9abb198e
soosap <prasath.soosaithasan@gmail.com>
2017-06-03 07:15:41 +0200

    Change addon order

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,7 +1,7 @@
 import '@storybook/addons';
+// Register knobs addon
+import '@storybook/addon-knobs/register';
 // Register actions addon
 import '@storybook/addon-actions/register';
-// Register knobs addon
-import '@storybook/addon-knobs/register'
 // Register addon to customize the storybook-ui
 import '@storybook/addon-options/register';
