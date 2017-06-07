6bde6d7e55d3152604993a761808222740a00a3f
soosap <prasath.soosaithasan@gmail.com>
2017-06-07 22:10:03 +0200

    Add fine-grained padding prop to <Segment />

--- src/components/layout/Segment/Segment.js
+++ src/components/layout/Segment/Segment.js
@@ -11,8 +11,12 @@ type Props = {
 };
 
 const getPadding = R.cond([
-  [R.propEq('padded', IntensityEnum.LIGHT), R.always('.5rem')],
-  [R.propEq('padded', IntensityEnum.STRONG), R.always('3rem')],
+  [R.propEq('padded', IntensityEnum.MINOR), R.always('.5rem')],
+  [R.propEq('padded', IntensityEnum.LIGHT), R.always('.75rem')],
+  [R.propEq('padded', IntensityEnum.MODERATE), R.always('1rem')],
+  [R.propEq('padded', IntensityEnum.STRONG), R.always('1.5rem')],
+  [R.propEq('padded', IntensityEnum.MAJOR), R.always('2rem')],
+  [R.propEq('padded', IntensityEnum.GREAT), R.always('3rem')],
   [R.propEq('padded', false), R.always('0')],
   [R.T, R.always('1.5rem')],
 ]);
