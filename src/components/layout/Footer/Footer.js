a9dfcbcd58e0bb8410d8c9d2c6d934a80ecf56b2
soosap <prasath.soosaithasan@gmail.com>
2017-06-08 14:25:37 +0200

    Add more props to <Footer /> component

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -7,10 +7,12 @@ import { Color } from '../../../lib/constants';
 type Props = {};
 
 const Footer = styled.footer`
-  color: ${Color.WHITE};
+  grid-area: footer;
+  color: ${Color.GRAY};
   background-color: ${Color.Black.LIGHT};
-  padding: 2rem;
-  grid-row: [footer-start] / [footer-end];
+  padding: 3rem 1.5rem 6rem;
+  font-weight: 400;
+  line-height: 1.5;
 `;
 
 export default (props: Props) => <Footer {...props} />;
--- src/components/layout/Footer/Footer.stories.js
+++ src/components/layout/Footer/Footer.stories.js
@@ -14,16 +14,31 @@ import {
 import centered from '@storybook/addon-centered';
 
 import { Footer } from '.';
-import { Wrapper } from '../Wrapper';
+import { Segment } from '../Segment';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+
+  grid-template-areas:
+    "content"
+    "footer";
+
+  grid-template-rows:
+    [content-start] 1fr
+    [content-end footer-start] auto
+    [footer-end]
+`;
+
+const Content = styled.div`
+  grid-area: content;
+`;
 
 const isNotNil = R.both(
   R.complement(R.isNil),
   R.complement(R.equals('undefined')),
 );
-
-const Content = styled.div`
-
-`;
 
 const stories = storiesOf('Footer', module);
 stories.addDecorator(withKnobs).addDecorator(centered);
@@ -36,8 +51,19 @@ stories.add('default', () => {
   return (
     <Wrapper>
       <Content>
-        <div>Hello</div>
-        <div>World</div>
+        <Segment>
+          <ul>
+            <li>Footer is a layout component.</li>
+            <li>
+              All layout components in saronia-ui are constrolled by data-grid.
+            </li>
+            <li>
+              A Footer component has a grid-area value of
+              {' '}<strong>footer</strong> applied by
+              default.
+            </li>
+          </ul>
+        </Segment>
       </Content>
       <Footer {...props}>
         Footer content
