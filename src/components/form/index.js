122696af3c1bd61cc6acf11bba1112c1f1d3c65f
soosap <prasath.soosaithasan@gmail.com>
2017-06-22 12:15:04 +0200

    Export <RadioGroup /> component

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -2,7 +2,7 @@
 import { Form } from './Form';
 import { Field, Fields, FieldLabel } from './Field';
 import { Input, TextArea } from './Input';
-import { Radio } from './Radio';
+import { Radio, RadioGroup } from './Radio';
 import { ContextMessage } from './ContextMessage';
 import { Checkbox, Checkboxes } from './Checkbox';
 
@@ -13,6 +13,7 @@ export {
   FieldLabel,
   Input,
   Radio,
+  RadioGroup,
   TextArea,
   ContextMessage,
   Checkbox,
